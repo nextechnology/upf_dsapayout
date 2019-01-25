c02763fbe1fa62284b22f5a4423ace6178ff8546
LENOVO <LENOVO@DESKTOP-50C5ATH>
2019-01-25 19:15:46 +0530

    dsa payout changes

--- upf-system-dsapayout/src/main/java/com/guavatrees/upf/controller/DSAPayoutController.java
+++ upf-system-dsapayout/src/main/java/com/guavatrees/upf/controller/DSAPayoutController.java
@@ -38,6 +38,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -3080,7 +3081,7 @@ public class DSAPayoutController {
 	
 	
 	@RequestMapping(value = "dsapayout", method = RequestMethod.GET)
-	public ModelAndView dsapayout() {
+	public ModelAndView dsapayout(@ModelAttribute("id") String id) {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("LOSjsp/dsapayout");
 		return mav;
